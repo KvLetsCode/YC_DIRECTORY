874e40157e3a2a63204ea393c7fd163fcc10564e
KrishanVeer Singh <122250944+KvLetsCode@users.noreply.github.com>
2025-08-24 16:53:33 +0530

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -20,7 +20,7 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
     <>
       <section className="pink_container">
         <p className="tag">PITCH, VOTE, AND GROW</p>
-        <h1 className="heading">Pitch Your Startup <br />, Connect With Entrepreneurs</h1>
+        <h1 className="heading">Pitch Your Startup, <br /> Connect With Entrepreneurs</h1>
         <p className="sub-heading !max-w-3xl" >
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
         </p>
