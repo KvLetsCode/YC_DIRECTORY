47ff7222b998b9bac33b1ca5f366fc29ba69ae42
kv <kvdata9@gmail.com>
2025-08-24 13:26:56 +0530

    first commit

---
+++ app/(root)/startup/create/page.tsx
@@ -0,0 +1,21 @@
+import Startupform from "@/components/Startupform";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+const Page = async () => {
+  const session = await auth();
+
+  if (!session) redirect("/");
+
+  return (
+    <>
+      <section className="pink_container !min-h-[230px]">
+        <h1 className="heading">Submit Your Startup</h1>
+      </section>
+
+      <Startupform />
+    </>
+  );
+};
+
+export default Page;
---
+++ components/SearchForm.tsx
@@ -0,0 +1,22 @@
+import Form from 'next/form'
+import SearchFormReset from './SearchFormReset'
+import { Search } from 'lucide-react'
+
+
+function SearchForm({query}:{query?: string}) {
+  return (
+      <Form action='/' scroll={false} className='search-form'>
+          <input type="text" name="query" className='search-input' defaultValue={query} placeholder='Search Startups' />
+          
+          <div className='flex gap-2'>
+              {query && <SearchFormReset />}
+              
+              <button type='submit' className='search-btn text-white cursor-pointer'>
+                  <Search />
+              </button>
+          </div>
+    </Form>
+  )
+}
+
+export default SearchForm
